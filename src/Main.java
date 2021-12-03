b6aa76ca44cabbad72b0329fe90acb1b95a72289
Yolan <alpha.nexus.280@gmail.com>
2021-12-03 14:05:09 -0500

    Adding Hero Constructor

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,26 @@
-import org.lwjgl.Sys;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
 
+
+        int largeurScreen = 1440;
+        int hauteurScreen = 768;
+        boolean isFullscreen = false;
+
         try {
-            AppGameContainer appGameContainer = new AppGameContainer(new Game("Zelda Dungeon"));
+            AppGameContainer app = new AppGameContainer(new GameLauncher("ZeldaDungeon"));
+            app.setDisplayMode(largeurScreen, hauteurScreen, isFullscreen);
+            app.setTargetFrameRate(60);
+
+            app.start();
         } catch (SlickException e) {
-            System.out.println();
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e);
         }
+
     }
-    
 }
