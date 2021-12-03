484d9312aa09a55962da43b6c2ec85d1185fb99e
KISNOC <robinmcd56@gmail.com>
2021-12-03 12:55:28 -0500

    Ajout 2eme Map

--- src/Game/Map/Map.java
+++ src/Game/Map/Map.java
@@ -4,14 +4,17 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
 
 public class Map {
-    private TiledMap map;
+    private TiledMap map1;
+    private TiledMap map2;
 
     public Map() throws SlickException {
-        this.map = new TiledMap(".ressources/maps/map1.tmx");
+        this.map1 = new TiledMap(".ressources/maps/map1.tmx");
+        this.map2 = new TiledMap(".ressources/maps/map2.tmx");
     }
 
+    //map1.render(0, 0);
+    //map2.render(0, 0);
     public Map(String name){
-        map.render(0, 0);
     }
 
     /*
