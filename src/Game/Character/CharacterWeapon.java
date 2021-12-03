d71fd5a8b336c6530c9f9607a6b9d81527e77e37
Yolan <alpha.nexus.280@gmail.com>
2021-12-03 14:07:04 -0500

    Construct body and basic structure

--- src/Game/Character/CharacterWeapon.java
+++ src/Game/Character/CharacterWeapon.java
@@ -1,9 +1,64 @@
 package Game.Character;
 
 public class CharacterWeapon {
-    private WeaponProjectile weaponProjectile;
+    private final String name;
+    private int damage;
+    private final boolean isProjectile;
 
-    public CharacterWeapon(){}
+    private CharacterWeapon(WeaponBuilder weaponBuilder){
+        this.name = weaponBuilder.name;
+        this.damage = weaponBuilder.damage;
+        this.isProjectile = weaponBuilder.isProjectile;
+    }
 
-    public CharacterWeapon(String name){}
+    public static WeaponBuilder builder(){
+        return new WeaponBuilder();
+    }
+
+    public static class WeaponBuilder{
+        private String name;
+        private int damage;
+        private boolean isProjectile;
+
+        public WeaponBuilder(){
+            this.name = "Super Arme";
+            this.damage = 20;
+            this.isProjectile = false;
+        }
+
+        public WeaponBuilder withName(String _name){
+            this.name = _name;
+            return this;
+        }
+
+        public WeaponBuilder withDamage(int _damage){
+            this.damage = _damage;
+            return this;
+        }
+
+        public WeaponBuilder isProjectile(){
+            this.isProjectile = true;
+            return this;
+        }
+
+        public CharacterWeapon build(){
+            return new CharacterWeapon(this);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public boolean isProjectile() {
+        return isProjectile;
+    }
 }
