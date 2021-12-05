d07faf8d222927a17e185e1f6f4f426e7ae1c7fe
Yolan <alpha.nexus.280@gmail.com>
2021-12-04 21:54:56 -0500

    Adding Image Manager

--- src/Game/Animation/AnimationManager.java
+++ src/Game/Animation/AnimationManager.java
@@ -15,14 +15,17 @@ import java.util.List;
 public class AnimationManager {
 
     private HashMap<GroupList, AnimationGroup> allAnimGroup = new HashMap<>();
+    private HashMap<String, GameImage> allImage = new HashMap<>();
 
     protected Image heroSprite;
+    protected Image heartSpriteFull;
+    protected Image getHeartSpriteEmpty;
 
     private static AnimationManager am;
 
 
     private AnimationManager() throws SlickException {
-        this.heroSprite = new Image("ressources/sprites/heroSprite.png");
+
     }
 
     public static AnimationManager getInstance() throws SlickException {
@@ -32,13 +35,27 @@ public class AnimationManager {
         return am;
     }
 
-    public void loadAnimations() throws SlickException {
+    public void load() throws SlickException {
+
+        this.heroSprite = new Image("ressources/sprites/heroSprite.png");
+
+
         this.allAnimGroup.put(GroupList.HERO, new AnimationGroup(GroupList.HERO, 0, 6, 3));
         this.allAnimGroup.put(GroupList.Enemy, new AnimationGroup(GroupList.Enemy, 4, 3, 3));
         this.allAnimGroup.put(GroupList.Boss, new AnimationGroup(GroupList.Enemy, 0, 3, 3));
+
+        this.allImage.put("heartFull", new GameImage("heartFull", new Image("ressources/sprites/heart32_32_FULL.png")));
+        this.allImage.put("heartEmpty", new GameImage("heartFull", new Image("ressources/sprites/heart32_32_EMPTY.png")));
     }
 
     public AnimationGroup getGroup(GroupList groupList){
         return this.allAnimGroup.get(groupList);
+    }
+
+    public GameImage getGameImage(String name){
+        if(this.allImage.containsKey(name)){
+            return this.allImage.get(name);
+        }
+        return null;
     }
 }
