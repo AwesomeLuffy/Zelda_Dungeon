22ef525c96b2d6d351dad393bf535f889d34d27b
Yolan <alpha.nexus.280@gmail.com>
2021-12-04 19:25:48 -0500

    Animation -> GameAnimation

--- src/Game/Character/Character.java
+++ src/Game/Character/Character.java
@@ -1,13 +1,16 @@
 package Game.Character;
 
+import Game.Animation.GameAnimation;
 import Game.Animations;
 import org.newdawn.slick.Animation;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Vector2f;
 
 public interface Character {
 
-    void draw(Animations animations);
-    void draw(Vector2f vector2f, Animations animations);
-    Animation getAnimation(Animations animations);
+    void draw() throws SlickException;
+    void draw(Animations animations) throws SlickException;
+    void draw(Vector2f vector2f, Animations animations) throws SlickException;
+    GameAnimation getAnimation(Animations animations) throws SlickException;
 
 }
