a9f2db64bd59b275e49cfb78c9f6794afc76392f
Yolan <alpha.nexus.280@gmail.com>
2021-12-04 21:55:25 -0500

    Adding Graphics parametter to draw

--- src/Game/Character/Character.java
+++ src/Game/Character/Character.java
@@ -3,14 +3,15 @@ package Game.Character;
 import Game.Animation.GameAnimation;
 import Game.Animations;
 import org.newdawn.slick.Animation;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Vector2f;
 
 public interface Character {
 
-    void draw() throws SlickException;
-    void draw(Animations animations) throws SlickException;
-    void draw(Vector2f vector2f, Animations animations) throws SlickException;
+    void draw(Graphics graphics) throws SlickException;
+    void draw(Graphics graphics, Animations animations) throws SlickException;
+    void draw(Graphics graphics, Vector2f vector2f, Animations animations) throws SlickException;
     GameAnimation getAnimation(Animations animations) throws SlickException;
 
 }
