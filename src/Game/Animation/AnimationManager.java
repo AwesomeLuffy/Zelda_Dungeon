74f2a32640e7c1b72a61dfb0719165adc4ea9790
Yolan <alpha.nexus.280@gmail.com>
2021-12-07 18:35:46 -0500

    Use GameAnimation builder

--- src/Game/Animation/AnimationManager.java
+++ src/Game/Animation/AnimationManager.java
@@ -1,23 +1,20 @@
 package Game.Animation;
 
-import Game.Animations;
 import Game.GroupList;
-import org.newdawn.slick.Animation;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.geom.Point;
 import org.newdawn.slick.geom.Vector2f;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class AnimationManager {
 
     private HashMap<GroupList, AnimationGroup> allAnimGroup = new HashMap<>();
     private HashMap<String, GameImage> allImage = new HashMap<>();
+    private HashMap<String, GameAnimation> uniqueAnim = new HashMap<>();
 
     protected Image heroSprite;
+    protected Image bulletSprite;
     protected Image heartSpriteFull;
     protected Image getHeartSpriteEmpty;
 
@@ -38,18 +35,36 @@ public class AnimationManager {
     public void load() throws SlickException {
 
         this.heroSprite = new Image("ressources/sprites/heroSprite.png");
+        this.bulletSprite = new Image("ressources/sprites/bulletSprite.png");
 
 
-        this.allAnimGroup.put(GroupList.HERO, new AnimationGroup(GroupList.HERO, 0, 6, 3));
-        this.allAnimGroup.put(GroupList.Enemy, new AnimationGroup(GroupList.Enemy, 4, 3, 3));
-        this.allAnimGroup.put(GroupList.Boss, new AnimationGroup(GroupList.Enemy, 0, 3, 3));
+        this.allAnimGroup.put(GroupList.HERO, new AnimationGroup(this.heroSprite, GroupList.HERO, 0, 6, 3, false));
+        this.allAnimGroup.put(GroupList.Enemy, new AnimationGroup(this.heroSprite, GroupList.Enemy, 4, 3, 3, false));
+        this.allAnimGroup.put(GroupList.Boss, new AnimationGroup(this.heroSprite, GroupList.Enemy, 0, 3, 3, false));
+
+        this.uniqueAnim.put("Bullet", GameAnimation.builder()
+                .withSprite(this.bulletSprite)
+                .withSpritePos(new Vector2f(0, 6))
+                .withDuration(100)
+                .withTileSize(32)
+                .withFrameNumber(4)
+                .withName("Bullet")
+                .isVertical()
+                .build());
 
         this.allImage.put("heartFull", new GameImage("heartFull", new Image("ressources/sprites/heart32_32_FULL.png")));
-        this.allImage.put("heartEmpty", new GameImage("heartFull", new Image("ressources/sprites/heart32_32_EMPTY.png")));
+        this.allImage.put("heartEmpty", new GameImage("heartEmpty", new Image("ressources/sprites/heart32_32_EMPTY.png")));
     }
 
     public AnimationGroup getGroup(GroupList groupList){
         return this.allAnimGroup.get(groupList);
+    }
+
+    public GameAnimation getUniqueAnimation(String name){
+        if(this.uniqueAnim.containsKey(name)) {
+            return this.uniqueAnim.get(name);
+        }
+        return null;
     }
 
     public GameImage getGameImage(String name){
