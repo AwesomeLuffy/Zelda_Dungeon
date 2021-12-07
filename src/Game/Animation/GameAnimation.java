ea8e9629338c3caa2b1d5e14f63046d3d4fd1edf
Yolan <alpha.nexus.280@gmail.com>
2021-12-07 18:36:25 -0500

    Creating GameAnimation builder pattern

--- src/Game/Animation/GameAnimation.java
+++ src/Game/Animation/GameAnimation.java
@@ -3,7 +3,6 @@ package Game.Animation;
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.geom.Point;
 import org.newdawn.slick.geom.Vector2f;
 
 public class GameAnimation{
@@ -11,26 +10,103 @@ public class GameAnimation{
     private final String name;
     private int frameNumber;
     private Animation animation;
-    private Image heroSprite;
+    private Image sprite;
+    private boolean isVertical;
+    private Vector2f spritePos;
+    private int duration;
 
 
-    public GameAnimation(String _name, int _tileSize, Vector2f vector2f, int _frameNumber) throws SlickException {
+    private GameAnimation(GameAnimationBuilder builder) throws SlickException {
 
-        this.name = _name;
-        this.tileSize = _tileSize;
-        this.frameNumber = _frameNumber;
+        this.name = builder.name;
+        this.tileSize = builder.tileSize;
+        this.frameNumber = builder.frameNumber;
+        this.sprite = builder.sprite;
+        this.isVertical = builder.isVertical;
+        this.spritePos = builder.spritePos;
+        this.duration = builder.duration;
 
 
-        this.constructAnim((int) vector2f.getX(), (int) vector2f.getY());
+        this.constructAnim((int) this.spritePos.getX(), (int) this.spritePos.getY(), this.isVertical);
     }
 
-    private void constructAnim(int x, int y) throws SlickException {
+    private void constructAnim(int x, int y, boolean isVertical) throws SlickException {
         this.animation = new Animation();
 
         for(int i = x; i < x + this.frameNumber; i++){
-            this.animation.addFrame(AnimationManager.getInstance().heroSprite.getSubImage(
-                    i * this.tileSize, y * this.tileSize, this.tileSize, this.tileSize), 300);
+            if(isVertical) {
+                this.animation.addFrame(this.sprite.getSubImage(
+                        y * this.tileSize, i * this.tileSize, this.tileSize, this.tileSize), this.duration);
+            }
+            else {
+                this.animation.addFrame(this.sprite.getSubImage(
+                        i * this.tileSize, y * this.tileSize, this.tileSize, this.tileSize), this.duration);
+            }
         }
+    }
+
+    public static GameAnimationBuilder builder(){
+        return new GameAnimationBuilder();
+    }
+
+    public static class GameAnimationBuilder{
+        private String name;
+        private int tileSize;
+        private int frameNumber;
+        private Animation animation;
+        private Image sprite;
+        private boolean isVertical;
+        private int duration;
+        private Vector2f spritePos;
+
+        public GameAnimationBuilder(){
+            this.tileSize = 32;
+            this.name = "";
+            this.frameNumber = 1;
+            this.sprite = null;
+            this.isVertical = false;
+            this.duration = 300;
+        }
+
+        public GameAnimationBuilder withName(String _name){
+            this.name = _name;
+            return this;
+        }
+
+        public GameAnimationBuilder withFrameNumber(int _number){
+            this.frameNumber = _number;
+            return this;
+        }
+
+        public GameAnimationBuilder withTileSize(int _number){
+            this.tileSize = 32;
+            return this;
+        }
+
+        public GameAnimationBuilder withSprite(Image _img){
+            this.sprite = _img;
+            return this;
+        }
+
+        public GameAnimationBuilder isVertical(){
+            this.isVertical = true;
+            return this;
+        }
+
+        public GameAnimationBuilder withDuration(int _duration){
+            this.duration = _duration;
+            return this;
+        }
+
+        public GameAnimationBuilder withSpritePos(Vector2f vector2f){
+            this.spritePos = vector2f;
+            return this;
+        }
+
+        public GameAnimation build() throws SlickException {
+            return new GameAnimation(this);
+        }
+
     }
 
     public Animation getAnimation(){
