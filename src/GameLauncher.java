654090de9b870076f021e992688a05ab7df91622
Yolan <alpha.nexus.280@gmail.com>
2021-12-11 20:37:05 -0500

    Adding attack system to GameLauncher

--- src/GameLauncher.java
+++ src/GameLauncher.java
@@ -2,9 +2,11 @@ import Game.*;
 
 import Game.Animation.AnimationManager;
 import Game.Character.CharacterWeapon;
+import Game.Character.Colision.GameCollisionManager;
 import Game.Character.Type.Hero;
 import Game.Character.Type.Enemy;
 import Game.Map.GameMap;
+import Game.Map.GameMapManager;
 import org.newdawn.slick.*;
 import org.newdawn.slick.geom.Vector2f;
 
@@ -17,15 +19,16 @@ public class GameLauncher extends BasicGame{
     private GameMap mapAcutely;
     private int state;
 
-    private Music music;
-    //private Vector2f position;
-
-    private AnimationManager am;
+    private AnimationManager am = AnimationManager.getInstance();
+    private GameCollisionManager gameCollisionManager = GameCollisionManager.getInstance();
 
     private Hero hero;
     private Enemy enemy;
 
-    private int inputPressed;
+
+    private int delta;
+
+    private boolean justAttack = false;
 
     private Animations animations;
 
@@ -43,25 +46,34 @@ public class GameLauncher extends BasicGame{
             System.out.println(e.toString());
         }
 
-        this.am = AnimationManager.getInstance();
         this.am.load();
 
         this.hero = Hero.builder()
                 .name("Light Knight")
                 .withLife(3)
                 .withWeapon(
-                        CharacterWeapon.builder().withName("Epée de Thanos").withDamage(75).build())
+                        CharacterWeapon.builder().withName("Epée de Thanos").withDamage(75).withAnimation("FireEnergy").build())
                 .build();
         this.hero.setCharacterPosition(new Vector2f(22,12));
 
         this.enemy = Enemy.builder().build();
         this.enemy.setCharacterPosition(new Vector2f(32,15));
 
-        this.music = GameSound.getInstance().getMusic();
+        this.gameCollisionManager.initCollides();
+
+        GameSound.getInstance().getMusic();
     }
 
     @Override
     public void update(GameContainer gameContainer, int i) throws SlickException {
+
+        if(this.delta >= this.hero.getCharacterWeapon().getAnimation().getTotalDuration()){
+            this.justAttack = (this.justAttack) ? false : this.justAttack;
+            this.delta = 0;
+        }
+
+        this.delta+=i;
+
         if (this.mapAcutely.changeMap(this.hero.getCharacterPosition())){
             if (this.mapAcutely == this.map1){
                 this.state = 2;
@@ -90,20 +102,35 @@ public class GameLauncher extends BasicGame{
                     this.hero.moveToUp(i);
                 }
             }
+            else if (UserInteraction.isSpacePressed(gameContainer) && !this.justAttack){
+                this.hero.attack(enemy);
+                this.justAttack = true;
+                this.delta = 0;
+            }
         }
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
         if (state == 1){
             this.mapAcutely = this.map1;
         }
         else {
             this.mapAcutely = this.map2;
         }
+
+
         this.mapAcutely.render();
         this.hero.draw(graphics);
         this.enemy.draw(graphics);
+
+        if (this.justAttack) {
+             this.hero.getCharacterWeapon().draw(this.hero.getCharacterPosition(), this.hero.getActualDirection());
+        }
+
+        this.gameCollisionManager.getGameColision(this.hero).drawRect(graphics);
+
     }
 
     public AnimationManager getAnimationManager(){
